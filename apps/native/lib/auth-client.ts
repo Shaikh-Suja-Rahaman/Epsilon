d459565dca2bfcb751065efe9e3dd2dcf25106c1
Shaikh-Suja-Rahaman <sujarahaman.dms.727@gmail.com>
2025-10-29 04:12:37 +0530

    test

--- apps/native/lib/auth-client.ts
+++ apps/native/lib/auth-client.ts
@@ -10,6 +10,7 @@ export const authClient = createAuthClient({
 			scheme: Constants.expoConfig?.scheme as string,
 			storagePrefix: Constants.expoConfig?.scheme as string,
 			storage: SecureStore,
+			// hsadgfhsdajgfhgsdfaskd
 		}),
 	],
 });
